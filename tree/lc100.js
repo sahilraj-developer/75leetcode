5d26830ccaa3c67eb9fbc6221cb36f4c3a062100
sahil <sahil27raj@gmail.com>
2025-11-20 23:58:18 +0530

    add the solution for the Problem: Same Tree (LC 100)   update the progress status

---
+++ tree/lc100.js
@@ -0,0 +1,12 @@
+Problem: Same Tree (LC 100)
+Question:
+
+Given the roots of two binary trees p and q,
+return true if the two trees are structurally identical and the nodes have the same values, otherwise false.
+
+✅ Example 1
+
+
+Input:
+p = [1,2,3], q = [1,2,3]
+Output: true
